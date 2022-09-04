269b6578b209c3956e6c2be099003a5d3f7577d2
kamakshinagar <knagar@heroku.com>
2022-09-03 17:04:33 -0700

    changed urls.py

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -17,5 +17,6 @@ import hello.views
 urlpatterns = [
     path("", hello.views.index, name="index"),
     path("db/", hello.views.db, name="db"),
+    path("rc/", hello.views.rc, name="rc"),
     path("admin/", admin.site.urls),
 ]
